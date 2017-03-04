49b559a6be9581a0a4f1b77d24973ee314226b2f
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-04 14:05:08 -0800

    yrp

--- src/routes.php
+++ src/routes.php
@@ -5,7 +5,7 @@
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
-
+    $test = "what";
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
