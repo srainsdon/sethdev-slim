1b336bd608d573af65be7b92583bc2158f83201d
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-04 21:14:51 -0800

    working on item

--- public/index.php
+++ public/index.php
@@ -26,12 +26,12 @@ $app = new \Slim\App($settings);
 $container = $app->getContainer();
 
 // Logger config
-$container['logger'] = function($c) {
-    $logger = new \Monolog\Logger('my_logger');
-    $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
-    $logger->pushHandler($file_handler);
-    return $logger;
-};
+//$container['logger'] = function($c) {
+//    $logger = new \Monolog\Logger('my_logger');
+//    $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
+//    $logger->pushHandler($file_handler);
+//    return $logger;
+//};
 
 // DB config
 $container['db'] = function ($c) {
