762760e435b6ffeee3bb0d94ba77fb2b46f227c1
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-04 19:21:17 -0800

    settings

--- src/db_settings.php
+++ src/db_settings.php
@@ -1,25 +1,19 @@
 <?php
 
-class AppData {
+$db_creds = unserialize(getenv('sethdev_DB'));
 
-    private $db_creds;
+$dsn = "mysql:host=" . $db_creds['host'] . ";dbname=" . $db_creds['database'] . ";charset=utf8";
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+$pdo = new PDO($dsn, $db_creds['username'], $db_creds['password'], $opt);
 
-    function __construct($param) {
-        $this->db_creds = unserialize(getenv('sethdev_DB'));
-
-        $dsn = "mysql:host=" . $db_creds['host'] . ";dbname=" . $db_creds['database'] . ";charset=utf8";
-        $opt = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false,
-        ];
-        $pdo = new PDO($dsn, $db_creds['username'], $db_creds['password'], $opt);
-
-        $urlSettings = array();
-        $stmt = $pdo->query('SELECT * FROM setting');
-        foreach ($stmt as $result) {
-            $urlSettings[$result['SettingName']] = $result['SettingValue'];
-        }
-    }
-
+$urlSettings = array();
+$stmt = $pdo->query('SELECT * FROM setting');
+foreach ($stmt as $result)
+{
+    $urlSettings[$result['SettingName']] = $result['SettingValue'];
 }
+return $urlSettings;
