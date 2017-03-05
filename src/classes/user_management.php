504951c1f4357d23e2793b477a7234446db17155
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-05 00:34:17 -0800

    User management

--- src/classes/user_management.php
+++ src/classes/user_management.php
@@ -17,7 +17,7 @@ class user_management {
     }
 
     function login(Psr\Http\Message\ServerRequestInterface $request, $response, $args) {
-        var_dump(json_encode($request));
+        var_dump(json_encode($request->getAttribute('params')));
 //        if ($request->isPost()) {
 //            $username = $request->post('UserName');
 //            $password = $request->post('Password');
