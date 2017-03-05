360b9d0355bc3d758b850fe28a6ed1a69c9120cc
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2017-03-04 20:05:55 -0800

    settings

--- src/routes.php
+++ src/routes.php
@@ -1,13 +1,13 @@
 <?php
 
 // Routes
-var_dump($app->getContainer()->routeSettings);
+$route_settings = $app->getContainer()->routeSettings;
 $app->get('/settings',
         function ($request, $response, $args) {
     var_dump($this->routeSettings);
 });
 
-$app->get('/item/[{id}]',
+$app->get('/' . $route_settings['ItemPage'] . '/[{id}]',
         function ($request, $response, $args) {
      $item['id'] = (int)$args['id'];
     // Render index view
